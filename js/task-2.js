3e9999c35bcf2070f43538569d6facba7d5f96d0
Alyona <olenapav97@gmail.com>
2024-09-02 22:18:19 +0300

    ff

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+makeArray(firstArray, secondArray, maxLength); {
+  const combinedArray = firstArray.concat(secondArray);
+  if (combinedArray.length > maxLength) {
+    return combinedArray.slice(0, maxLength);
+  }
+  return combinedArray;
+}
